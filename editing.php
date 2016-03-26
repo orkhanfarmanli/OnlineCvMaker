3f7666791405e79c4ca2547d6b5389f138ed6130
Vahab Valiyev <vahab.v@code.edu.az>
2016-03-26 02:26:09 -0700

    Project started, index page ready, navbar ready in select.php, style.css is common css file which compiled from index.scss(sass)

---
+++ editing.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online CV Generator</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+</body>
+</html>
---
+++ select.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online CV Generator</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<ul id="dropdown1" class="dropdown-content">
+			  <li><a href="#!">one</a></li>
+			  <li><a href="#!">two</a></li>
+			  <li class="divider"></li>
+			  <li><a href="#!">three</a></li>
+	</ul>
+  <nav>
+  <div class="container">
+    <div class="row">
+	    <div class="nav-wrapper">
+	      <a href="#!" class="brand-logo"><b>RESUME</b>CREATOR</a>
+	     <ul class="right hide-on-med-and-down">
+      			<li><a class="dropdown-button userBar" href="#!" data-activates="dropdown1"><img src="images/resumes.png"> Vahab Valiyev<i class="material-icons right">arrow_drop_down</i></a></li>
+   		 </ul>
+	    </div>
+	  </div>
+	</div>
+  </nav>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
