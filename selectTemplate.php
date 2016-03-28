707f4555e4746483e69d7f53817197a4c7bc283d
Farida Samadzadeh <farida.s@code.edu.az>
2016-03-28 17:47:40 +0500

    Added select template page

---
+++ selectTemplate.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online CV Generator</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/design.css">
+</head>
+<body>
+	<ul id="dropdown1" class="dropdown-content">
+			  <li><a href="#!">one</a></li>
+			  <li><a href="#!">two</a></li>
+			  <li class="divider"></li>
+			  <li><a href="#!">three</a></li>
+	</ul>
+  <nav>
+  <div class="container">
+    <div class="row">
+	    <div class="nav-wrapper">
+	      <a href="#!" class="brand-logo"><b>RESUME</b>CREATOR</a>
+	     <ul class="right hide-on-med-and-down">
+      			<li><a class="dropdown-button userBar" href="#!" data-activates="dropdown1"><img src="images/resumes.png"> Vahab Valiyev<i class="material-icons right">arrow_drop_down</i></a></li>
+   		 </ul>
+	    </div>
+	  </div>
+	</div>
+  </nav>
+  <div class="row words" ><h6>SELECT YOUR TEMPLATE</h6></div>
+<div class="divler row">
+	<div class="window z-depth-2 col s12 m3"></div>
+	<div class="window z-depth-2 col s12 m3 offset-m1"></div>
+	<div class="window z-depth-2 col s12 m3 offset-m1"></div>
+</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
