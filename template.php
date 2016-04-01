f834d0fc8e199670e42fec822922aec31582b84e
Vahab Valiyev <vahab.v@code.edu.az>
2016-04-01 08:16:27 -0700

    struktur seliqeye salindi

--- template.php
+++ template.php
@@ -1,14 +1,10 @@
-﻿<?php 
-include "app/model.php";
-$newDb= new Database('localhost','root','','usercv');
- ?>
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>Template</title>
-	<link rel="stylesheet" href="css/style.css" type="style/css">
+  <title>Template</title>
+  <link rel="stylesheet" href="css/style.css" type="style/css">
 
-	<!--Import materialize.css-->
+  <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
   <!-- For  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -18,9 +14,8 @@ $newDb= new Database('localhost','root','','usercv');
 <body>
 
   <!--Import jQuery before materialize.js-->
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-
+  <!-- <script type="text/javascript" src="js/jquery.js"></script>-->
+  
 
   <div class="">
     <div class="row">
@@ -30,10 +25,10 @@ $newDb= new Database('localhost','root','','usercv');
           <div class="card-content grey-text">
             <span >Select color</span>
             <ul class="pagination">
-              <li class="waves-effect"><a href="#!"><div class="teal ball"></div></a></li>
-              <li class="waves-effect"><a href="#!"><div class="blue ball"></div></a></li>
-              <li class="waves-effect"><a href="#!"><div class="red ball"></div></a></li>
-              <li class="waves-effect"><a href="#!"><div class="purple ball"></div></a></li>
+              <li class="waves-effect"><a href="#!"><div class="teal ball" name="teal-text"></div></a></li>
+              <li class="waves-effect"><a href="#!"><div class="blue ball" name="blue-text"></div></a></li>
+              <li class="waves-effect"><a href="#!"><div class="red ball" name="red-text"></div></a></li>
+              <li class="waves-effect"><a href="#!"><div class="purple ball" name="purple-text"></div></a></li>
             </ul>
             <a href="#" class="btn grey btn-text"> <i class="material-icons left">loop</i>Change design</a>
             <a href="#" class="btn  grey btn-text"> <i class="material-icons left">description</i>Save as draft</a>
@@ -47,9 +42,237 @@ $newDb= new Database('localhost','root','','usercv');
 
 
 
-<?php 
-$showCv = new ShowCv($newDb->connection);
- ?>
+      <div class="col s12 m9">
+
+
+       <div class="col s12 m3">
+        <div class="card">
+          <div class="card-image">
+            <img src="images/1.png" class="responsive-img">
+          </div>
+          <div class="card-content">
+            <blockquote class=" grey-text">
+              +994 55 885 73 09
+            </blockquote>
+
+            <blockquote class=" grey-text">
+              knyaz.yaqublu@gmail.com
+            </blockquote>
+
+            <blockquote class=" grey-text">
+              twitter.com@knyaz
+            </blockquote>
+
+            <blockquote class=" grey-text">
+              www.knyazik.com
+            </blockquote>
+
+            <blockquote class=" grey-text">
+              Ataturk ave.33 Baku Azerbaijan
+            </blockquote>
+            <div class="languages">
+              <i class="material-icons left grey-text">language</i><h5 class="blue-text " name="desingText">LANGUAGES</h5>
+
+              <span class="grey-text text-darken-2">ENGLISH</span> <i class="material-icons tiny right blue-text"  name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+
+              <span class="grey-text text-darken-2">RUSSIAN</span> <i class="material-icons tiny right blue-text" name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+
+          <span class="grey-text text-darken-2">SPANISH</span> <i class="material-icons tiny right blue-text"  name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+
+              <span class="grey-text text-darken-2">TURKISH</span> <i class="material-icons tiny right blue-text"  name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text  " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+            
+            </div>
+           
+            <div class="skills">
+              
+
+           
+            
+              <i class="material-icons left grey-text">list</i><h5 class="blue-text  " name="desingText">SKILLS</h5>
+              <span class="grey-text text-darken-2">PHOTOSHOP</span> <i class="material-icons tiny right blue-text" name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+
+              <span class="grey-text text-darken-2">HTML/CSS</span> <i class="material-icons tiny right blue-text" name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+
+              <span class="grey-text text-darken-2">ILLUSTRATOR</span> <i class="material-icons tiny right blue-text" name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+
+              <span class="grey-text text-darken-2">SKETCH</span> <i class="material-icons tiny right blue-text"  name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+              <i class="material-icons tiny right blue-text " name="desingText">stars</i>
+
+
+             </div>
+             <div class="awards">
+
+              <i class="material-icons left grey-text">grade</i><h5 class="blue-text" name="desingText">AWARDS</h5>
+              <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro. Earum pariatur hic eaque, est architecto odit quae cupiditate?Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <a class="btn grey black-text"><i class="material-icons center">add</i> <span>ADD MORE</span></a>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+      <div class="col s12 m9 right white">
+        <div class="card white darken-1">
+          <div class="card-content grey-text">
+            <h2 class=" blue-text text-darken-3" name="desingText"><b>KNYAZ YAQUBOV</b></h2>
+            <h5 class="grey-text"><b>GRAPHIC DESIGNER</b></h5>
+            <h6 class="grey-text">Date of birth: 02.06.1989</h6>
+
+            <p class="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia 
+              perspiciatis est porro. Earum pariatur hic eaque, est architecto odit quae cupiditate?Lorem ipsum dolor sit amet,
+               consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro. 
+               Earum pariatur hic eaque, est architecto odit quae cupiditate?Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro. Earum pariatur hic eaque, est
+                 architecto odit quae cupiditate?</p>
+
+
+
+                <div class="row">
+                  
+                   <div class="edit">
+                    <form action="">
+                      <fieldset>
+                        <legend>
+                          <ul class="fieldset">
+                            <li><a class="btn"><i class="material-icons">edit</i></a></li>
+                            <li> <a class="btn waves-effect waves-light"><i class="material-icons">keyboard_arrow_up</i></a></li>
+                            <li> <a class="btn"><i class="material-icons">keyboard_arrow_down</i></a></li>
+                          </ul>
+                         
+                         
+                         
+                        </legend>
+
+                            <i class="material-icons left grey-text">school</i><h5 class="blue-text" name="desingText">EDUCATION</h5>
+                    <div class="row"> 
+                      <span class=" grey-text text-darken-2"><b>2012-2013 AZƏRBAYCAN DÖVLƏT PEDAQOJİ UNİVERSİTETİ
+                    </b></span> <br>
+                    <span class="grey-text text-darken-2">BAKALAVR</span>
+                    <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
+                     a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro.</p>
+                    </div>
+                     
+
+                     <div class="row"> 
+                      <span class=" grey-text text-darken-2"><b>2012-2013 AZƏRBAYCAN DÖVLƏT PEDAQOJİ UNİVERSİTETİ</b></span> <br>
+                      <span class="grey-text text-darken-2">BAKALAVR</span>
+                      <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto
+                       a aspernatur ratione eos officia perspiciatis est porro.</p>
+                    </div>
+                      <div class="row"> 
+                      <span class=" grey-text text-darken-2"><b>2012-2013 AZƏRBAYCAN DÖVLƏT PEDAQOJİ UNİVERSİTETİ</b></span> <br>
+                      <span class="grey-text text-darken-2">BAKALAVR</span>
+                      <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto 
+                        a aspernatur ratione eos officia perspiciatis est porro.</p>
+
+
+                      </fieldset>
+
+                    </form>
+                    
+                    </div>
+
+
+            </div>
+                </div>
+                 
+                
+            <div class="row">
+              
+                <div class="edit">
+                  <form action="">
+                      <fieldset>
+                        <legend>
+                          <ul class="fieldset">
+                            <li><a class="btn"><i class="material-icons">edit</i></a></li>
+                            <li> <a class="btn waves-effect waves-light"><i class="material-icons">keyboard_arrow_up</i></a></li>
+                            <li> <a class="btn"><i class="material-icons">keyboard_arrow_down</i></a></li>
+                          </ul>
+                         
+                         
+                         
+                        </legend>
+              <i class="material-icons left grey-text">work</i><h5 class="blue-text " name="desingText">WORK EXPERIENCE</h5>
+              <div class="row">
+                <span class=" grey-text text-darken-2"><b>2012-2013 MICROSOFT CORPORATION</b></span> <br>
+              <span class="grey-text text-darken-2">GRAPHIC AND WEB DESIGN</span>
+              <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto
+               a aspernatur ratione eos officia perspiciatis est porro.</p> 
+
+              </div>
+              <div class="row">
+                 <span class="grey-text text-darken-2"><b>2012-2013 APPLE INC  </b></span> <br>
+                <span class="grey-text text-darken-2">GRAPHIC AND WEB DESIGN</span>
+                <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto
+                 a aspernatur ratione eos officia perspiciatis est porro.</p> 
+
+              </div>
+
+             
+
+              <div class="row">
+                <span class=" grey-text text-darken-2"><b>2012-2013 MICROSOFT CORPORATION</b></span> <br>
+                <span class="grey-text text-darken-2">GRAPHIC AND WEB DESIGN</span>
+                <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro.</p> 
+
+              </div>
+
+              <div class="row">
+                <span class=" grey-text text-darken-2"><b>2012-2013 MICROSOFT CORPORATION</b></span> <br>
+                <span class="grey-text text-darken-2">GRAPHIC AND WEB DESIGN</span>
+                <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae a ipsa fugit. Iusto a aspernatur ratione eos officia perspiciatis est porro.</p> 
+
+              </div>
+                  </fieldset>
+                </form>
+            </div>
+
+            </div>
+            
+
+            <a class="btn grey black-text"><i class="material-icons center">add</i> <span>ADD MORE</span></a>
+
+            </div>
+           
+          </div>
+        </div>
+      </div>
 
 
 
@@ -59,6 +282,40 @@ $showCv = new ShowCv($newDb->connection);
 
 
   </div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
 
+<script type="text/javascript">
+  
+  $(document).ready(
+
+      function(){
+        $(".ball").click(function(){
+             
+              var a = $(this).attr("name");
+             $("[name=desingText]").removeClass("red-text blue-text teal-text purple-text");
+          
+
+          $("[name=desingText]").addClass(a);
+
+          
+       
+        });
+
+       $(".edit").mouseover(function(){
+        $("fieldset",this).addClass("editBorder");
+        $("legend",this).show();
+      });
+      $(".edit").mouseout(function(){
+        $("fieldset",this).removeClass("editBorder");
+        $("legend",this).hide();
+      });
+      var width=$(".edit").width();
+      $("legend").css("margin-left",width*0.9+"px");
+
+      }
+      );
+
+</script>
 </body>
 </html>
