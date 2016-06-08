53c9c0dc27368fce4cb6e75232a9ccbb5fb62339
vahabv <vahab.v@code.edu.az>
2016-06-08 17:30:33 +0400

    migration problem trying to solve

--- database/migrations/2016_06_08_105325_create_works_table.php
+++ database/migrations/2016_06_08_105325_create_works_table.php
@@ -20,7 +20,8 @@ class CreateWorksTable extends Migration
             $table->string('work_company', 255);
             $table->string('work_profession', 255);
             $table->text('work_info');
-            $table->timestamps();
+                        $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+$table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
